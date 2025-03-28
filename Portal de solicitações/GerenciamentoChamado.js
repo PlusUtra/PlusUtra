62af7ecfc2f1d33bce4b5b5d15d6bf685be518d3
PlusUtra <94576479+PlusUtra@users.noreply.github.com>
2025-03-28 11:07:17 -0300

    Update GerenciamentoChamado.js

--- Portal de solicitações/GerenciamentoChamado.js
+++ Portal de solicitações/GerenciamentoChamado.js
@@ -2,17 +2,17 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.3.0/firebase-app.js";
 import { getDatabase, ref, get, set, push, remove } from "https://www.gstatic.com/firebasejs/11.3.0/firebase-database.js";
 
-// Configuração do Firebase
 const firebaseConfig = {
-    apiKey: "AIzaSyBbiG8N7mdznEZxpE9p0zlWasNT6bGDB3E",
-    authDomain: "oms-a-c9762.firebaseapp.com",
-    databaseURL: "https://oms-a-c9762-default-rtdb.firebaseio.com",
-    projectId: "oms-a-c9762",
-    storageBucket: "oms-a-c9762.firebasestorage.app",
-    messagingSenderId: "571808116138",
-    appId: "1:571808116138:web:3064f5406322204299cfd3",
-    measurementId: "G-Q98PRFJMYN"
+    apiKey: "-",
+    authDomain: "-",
+    databaseURL: "-",
+    projectId: "-",
+    storageBucket: "-",
+    messagingSenderId: "-",
+    appId: "-",
+    measurementId: "-"
 };
+
 
 // Inicializando o Firebase
 const app = initializeApp(firebaseConfig);
